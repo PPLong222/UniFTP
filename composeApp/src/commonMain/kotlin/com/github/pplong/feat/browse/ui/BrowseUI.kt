9a9603764cc8afb4fe1ab4f24b6b4e905bb1ac81
Luke Z <pplong222@outlook.com>
2025-10-17 15:06:29 -0400

    [optimize]: update main page loading icon layout

--- composeApp/src/commonMain/kotlin/com/github/pplong/feat/browse/ui/BrowseUI.kt
+++ composeApp/src/commonMain/kotlin/com/github/pplong/feat/browse/ui/BrowseUI.kt
@@ -2,8 +2,10 @@ package com.github.pplong.feat.browse.ui
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -23,6 +25,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -163,7 +166,10 @@ fun BrowseMainContent(
 ) {
     when (uiState.requestStatus) {
         CommonRequestStatus.INITIAL, CommonRequestStatus.REQUESTING -> {
-            ContainedLoadingIndicator(modifier = Modifier.size(128.dp))
+
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
+                ContainedLoadingIndicator(modifier = Modifier.padding(top = 100.dp).size(108.dp))
+            }
         }
 
         CommonRequestStatus.SUCCESS -> {
